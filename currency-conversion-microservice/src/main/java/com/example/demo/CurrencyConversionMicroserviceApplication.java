900534ff50c8fbfc256e2fbc41970374028eac38
sahil arora <sahilarora358@gmail.com>
2019-02-03 15:25:58 +0530

    Implemented Profiling

--- currency-conversion-microservice/src/main/java/com/example/demo/CurrencyConversionMicroserviceApplication.java
+++ currency-conversion-microservice/src/main/java/com/example/demo/CurrencyConversionMicroserviceApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,10 +12,10 @@ import com.example.controller.CurrencyConversionController;
 @EnableFeignClients("com.example.controller")
 @EnableDiscoveryClient
 @ComponentScan(basePackageClasses = CurrencyConversionController.class)
+@EnableAutoConfiguration
 public class CurrencyConversionMicroserviceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionMicroserviceApplication.class, args);
 	}
-
 }
